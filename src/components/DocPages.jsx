044ff4402a018c30e318659d2bea971dbd98f1bf
hudsonjhonson9-arch <hudsonjhonson9@gmail.com>
2026-09-15 13:48:31 +0800

    Update Google Docs viewer URL for PDF files

--- src/components/DocPages.jsx
+++ src/components/DocPages.jsx
@@ -866,7 +866,7 @@ export function DocDetail({ doc, onBack, onApprove, onReject, onDownload, onPrev
                 if (fid) return `https://drive.google.com/embeddedfolderview?id=${fid}#list`;
                 const fileId = extractGDriveFileId(doc.url);
                 // ponytail: Google Docs viewer shows PDF with page navigation sidebar
-                return `https://docs.google.com/gview?url=${encodeURIComponent(gdriveDirectUrl(fileId))}&embedded=true`;
+                return `https://drive.google.com/file/d/${fileId}/preview`
               })()}
               title={doc.title}
               style={{ width: "100%", flex: 1, minHeight: 600, border: "none", borderRadius: `${T.radius}px ${T.radius}px 0 0` }}
